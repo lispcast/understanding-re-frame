cc093f778f7bec9dd195943a272ec88e23f77037
Eric Normand <ericwnormand@gmail.com>
2017-11-02 14:42:16 -0500

    Add style attribute.

--- src/cljs/understanding_re_frame/hiccup.cljs
+++ src/cljs/understanding_re_frame/hiccup.cljs
@@ -2,8 +2,12 @@
   (:require [reagent.core :as reagent]))
 
 (defn hiccup-panel []
-  (let [title "Hello"]
-    [:div {:class "content"}
+  (let [title "Hello"
+        border-color "green"]
+    [:div {:class "content"
+           :style {:padding-left "10%"
+                   :border (when border-color
+                             (str "1px solid " border-color))}}
      [:h1 "Hiccup"]
      [:p "This is a message"]
      [:input {:type :checkbox
