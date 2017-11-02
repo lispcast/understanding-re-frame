5d106dbaf414fbf464a712a5ed6c136553624d68
Eric Normand <ericwnormand@gmail.com>
2017-11-02 14:50:46 -0500

    Add class pattern

--- src/cljs/understanding_re_frame/hiccup.cljs
+++ src/cljs/understanding_re_frame/hiccup.cljs
@@ -1,15 +1,25 @@
 (ns understanding-re-frame.hiccup
-  (:require [reagent.core :as reagent]))
+  (:require [reagent.core :as reagent]
+            [clojure.string :as str]))
+
+(defn cs [& args]
+  (str/join " " (map name (filter identity args))))
 
 (defn hiccup-panel []
   (let [title "Hello"
-        border-color "green"]
-    [:div {:class "content"
+        border-color "green"
+        active? true]
+    [:div {:class (cs
+                    "content"
+                    "row"
+                    (when active? "active"))
            :style {:padding-left "10%"
                    :border (when border-color
                              (str "1px solid " border-color))}}
      [:h1 "Hiccup"]
-     [:p "This is a message"]
+     [:p
+      {:class "first-paragraph bold-text"}
+      "This is a message"]
      [:input {:type :checkbox
               :checked false}]
      [:p "Another paragraph."]
