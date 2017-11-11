65cdcfc056ddf0e9a629245d1d85f5f8345643c7
Eric Normand <ericwnormand@gmail.com>
2017-11-10 23:14:13 -0600

    Add re-rendering experiments

--- src/cljs/understanding_re_frame/atoms.cljs
+++ src/cljs/understanding_re_frame/atoms.cljs
@@ -6,13 +6,23 @@
             [ajax.core :as ajax]))
 
 (defonce my-state (reagent/atom {}))
+(defonce my-other (reagent/atom {}))
 
-
+(defn sub-component [a]
+  (let [s (reagent/atom 0)]
+    (js/setInterval #(swap! s inc) 500)
+    (fn [a]
+      (js/console.log "SUB-COMPONENT")
+      [:div
+       [:div a]
+       [:div @s]
+       [:div @my-other]])))
 
 (defn atoms-panel []
+  (js/console.log "ATOMS-PANEL")
   [:div
    [:h1 "Atoms"]
-   
+   [sub-component (:name @my-state)]
    [:div
     (pr-str @my-state)]])
 
@@ -21,8 +31,11 @@
   (reset! my-state {:name "Eric"})
   (reset! my-state {})
 
+  (reset! my-other "Hello")
+
   (swap! my-state inc)
   (swap! my-state assoc :age 36)
+  (swap! my-state assoc :name "Eric")
 
   (swap! my-state update :age inc)
 
