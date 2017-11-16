eb8620bf04c8a1a66a655fcb93df1db838669583
Eric Normand <ericwnormand@gmail.com>
2017-11-16 13:04:19 -0600

    Add Event Handlers.

--- src/cljs/understanding_re_frame/re_frame_events.cljs
+++ src/cljs/understanding_re_frame/re_frame_events.cljs
@@ -11,6 +11,34 @@
   ;; fake method
   )
 
+(rf/reg-event-db
+  :notify-error
+  (fn [db [_ resp]]
+    (assoc db :error (:error resp))))
+
+(rf/reg-event-db
+  :add-cart-confirm
+  (fn [db [_ item-id]]
+    (let [items (get-in db [:cart :items])
+          items (mapv (fn [item]
+                        (if (= (:item item) item-id)
+                          (assoc item :confirmed? true)
+                          item))
+                  items)]
+      (assoc-in db [:cart :items] items))))
+
+(rf/reg-event-fx
+  :buy-product
+  (fn [cofx [_ item-id]]
+    {:db (update-in (:db cofx)
+           [:cart :items] conj {:item item-id
+                                :confirmed? false})
+     :http-xhrio {:uri (str "http://url.com/product/" item-id "/puchase")
+                  :method :post
+                  :response-format (ajax/json-response-format {:keywords? true})
+                  :on-success [:add-cart-confirm item-id]
+                  :on-failure [:notify-error]}}))
+
 (defn buy-button [item-id]
   [:button
    {:on-click (fn [e]
