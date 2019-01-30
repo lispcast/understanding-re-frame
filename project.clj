cbc013765bfb4eb2e761e9f37ab79405e0750332
Eric Normand <ericwnormand@gmail.com>
2019-01-30 10:20:38 -0600

    Update Clojure, ClojureScript, and Figwheel versions

--- project.clj
+++ project.clj
@@ -1,81 +1,15 @@
-(defproject understanding-re-frame "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/clojurescript "1.10.339"]
-                 [reagent "0.7.0"]
-                 [re-frame "0.10.2"]
-                 [secretary "1.2.3"]
-                 [compojure "1.5.0"]
-                 [yogthos/config "0.8"]
-                 [ring "1.4.0"]
-                 [day8.re-frame/http-fx "0.1.4"]]
+(defproject introduction-to-clojure "0.1.0-SNAPSHOT"
+  :description "Part of PurelyFunctional.tv"
+  :url "https://purelyfunctional.tv/"
+  :license {:name "CC0 1.0 Universal (CC0 1.0) Public Domain Dedication"
+            :url "http://creativecommons.org/publicdomain/zero/1.0/"}
+  :plugins [[nightlight/lein-nightlight "2.3.2"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [lispcast/bakery "1.0.0"]]
+  :repl-options {:init (do
+                         (use 'bakery.core)
+                         (use 'introduction-to-clojure.core))
+                 :skip-default-init true}
+  
 
-  :plugins [[lein-cljsbuild "1.1.5"]]
-
-  :min-lein-version "2.5.3"
-
-  :source-paths ["src/clj"]
-
-  :clean-targets ^{:protect false} ["resources/public/js/compiled" "target"
-                                    "test/js"]
-
-  :figwheel {:css-dirs ["resources/public/css"]
-             :ring-handler understanding-re-frame.handler/dev-handler}
-
-  :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
-
-  :profiles
-  {:dev
-   {:dependencies [[binaryage/devtools "0.9.4"]
-                   [cljsjs/d3 "4.3.0-5"]
-                   [day8.re-frame/trace "0.1.7"]
-                   
-                   [figwheel-sidecar "0.5.16"]
-                   [com.cemerick/piggieback "0.2.2"]
-                   [re-frisk "0.5.0"]]
-
-    :plugins      [[lein-figwheel "0.5.16"]
-                   [lein-doo "0.1.8"]]}}
-
-  :cljsbuild
-  {:builds
-   [{:id           "dev"
-     :source-paths ["src/cljs"]
-     :figwheel     {:on-jsload "understanding-re-frame.core/mount-root"}
-     :compiler     {:main                 understanding-re-frame.core
-                    :output-to            "resources/public/js/compiled/app.js"
-                    :output-dir           "resources/public/js/compiled/out"
-                    :asset-path           "js/compiled/out"
-                    :source-map-timestamp true
-                    :preloads             [devtools.preload
-                                           day8.re-frame.trace.preload
-                                           re-frisk.preload
-                                           ]
-                    :closure-defines      {"re_frame.trace.trace_enabled_QMARK_" true}
-                    :external-config      {:devtools/config {:features-to-install :all}}
-                    }}
-
-    {:id           "min"
-     :source-paths ["src/cljs"]
-     :jar true
-     :compiler     {:main            understanding-re-frame.core
-                    :output-to       "resources/public/js/compiled/app.js"
-                    :optimizations   :advanced
-                    :closure-defines {goog.DEBUG false}
-                    :pretty-print    false}}
-
-    {:id           "test"
-     :source-paths ["src/cljs" "test/cljs"]
-     :compiler     {:main          understanding-re-frame.runner
-                    :output-to     "resources/public/js/compiled/test.js"
-                    :output-dir    "resources/public/js/compiled/test/out"
-                    :optimizations :none}}
-    ]}
-
-  :main understanding-re-frame.server
-
-  :aot [understanding-re-frame.server]
-
-  :uberjar-name "understanding-re-frame.jar"
-
-  :prep-tasks [["cljsbuild" "once" "min"] "compile"]
-  )
+  :main introduction-to-clojure.core)
